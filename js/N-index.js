4c3f2d64f9065b0351c1bff13a1b48c3a55138d5
421538074 <421538074@qq.com>
2019-05-06 17:12:03 +0800

    样式修改

--- js/N-index.js
+++ js/N-index.js
@@ -174,10 +174,10 @@ var vm = avalon.define({
     //产看文章
     if (this.postIndex != index) {
       this.postIndex = index;
-      $(".content_list:eq("+index+") .mid_img").css("display","none")
+      $(".content_list:eq(" + index + ") .mid_img").css("display", "none")
     } else {
       this.postIndex = -1;
-      $(".content_list:eq("+index+") .mid_img").css("display","block")
+      $(".content_list:eq(" + index + ") .mid_img").css("display", "block")
 
     }
   },
@@ -652,7 +652,7 @@ var vm = avalon.define({
   },
   getCharaLength: function getCharaLength(str) {
     return str.replace(/[\u0391-\uFFE5]/g, "aa").length > 239;
-    
+
   },
   filterImg: function filterImg(content) {
     var reg = /style="[^\"]*?"/g;
@@ -727,6 +727,39 @@ var vm = avalon.define({
 jQuery.support.cors = true
 vm.created();
 
+
+avalon.filters.filterTime = function (time) {
+  var date = new Date(time * 1000);
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  if (day < 10) {
+    day = "0" + day;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+}
 
 
 //封装一个限制字数方法
