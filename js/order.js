972f25fc4bf43fe68f561c184de44244f7069bc0
421538074 <421538074@qq.com>
2019-05-09 10:07:34 +0800

    接单后局部刷新

--- js/order.js
+++ js/order.js
@@ -15,7 +15,7 @@ var vm = avalon.define({
   totalone: 8,
   // 记录总条数
   currentone: 1,
-  totalNum:0,
+  totalNum: 0,
   // 当前的页数
   onli: function onli(type) {
     var _this = this;
@@ -30,14 +30,14 @@ var vm = avalon.define({
         url: "".concat(api, "/index/api/repairLists"),
         async: true,
         data: {
-          page:1
+          page: 1
         },
         dataType: 'json',
         success: function success(res) {
           _this.list = res.data.list;
           _this.totalNum = res.data.num;
           avalon.ready(function () {
-              initPagation(type);
+            initPagation(type);
           });
         }
       });
@@ -48,13 +48,13 @@ var vm = avalon.define({
         url: "".concat(api, "/index/api/myRepairs"),
         async: true,
         data: {
-          page:1
+          page: 1
         },
         dataType: 'json',
         success: function success(res) {
           _this.list1 = res.data;
           avalon.ready(function () {
-              initPagation(type);
+            initPagation(type);
           });
         }
       });
@@ -62,7 +62,6 @@ var vm = avalon.define({
   },
   Repair: function Repair(index) {
     var _this2 = this;
-
     //接单
     $.ajax({
       type: "post",
@@ -76,18 +75,24 @@ var vm = avalon.define({
         console.log(res);
 
         if (res.code == 1) {
-          warn.alert(res.msg);
           $.ajax({
             type: "post",
             url: "".concat(api, "/index/api/repairLists"),
             async: true,
-            data: {},
+            data: {
+              page: 1
+            },
             dataType: 'json',
             success: function success(res) {
               _this2.list = res.data.list;
               _this2.totalNum = res.data.num;
+              avalon.ready(function () {
+                initPagation(type);
+              });
             }
           });
+          warn.alert(res.msg);
+
         } else {
           warn.alert(res.msg);
         }
@@ -130,7 +135,6 @@ var vm = avalon.define({
   },
   created: function created() {
     this.onli(0);
-    console.log(111111)
   },
 })
 
@@ -174,7 +178,7 @@ avalon.filters.filterTime = function (time) {
 
 function initPagation(type) {
   var url = "".concat(api, "/index/api/repairLists");
-  if(type != 0) {
+  if (type != 0) {
     url = "".concat(api, "/index/api/myRepairs");
   }
   $(".zxf_pagediv").createPage({
